761da898a23e282ccb8c5f7169422ea83ceb3298
Berta György <111363208+bertagyorgy@users.noreply.github.com>
2023-11-29 14:47:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import plotly.express as px
 import streamlit as st
+import matplotlib.pyplot as plt
 
-from streamlit_plotly_events import plotly_events
+#create your figure and get the figure object returned
+fig = plt.figure() 
+plt.plot([1, 2, 3, 4, 5]) 
 
-# Writes a component similar to st.write()
-fig = px.line(x=[1], y=[1])
-selected_points = plotly_events(fig)
-
-# Can write inside of things using with!
-with st.expander('Plot'):
-    fig = px.line(x=[1], y=[1])
-    selected_points = plotly_events(fig)
-
-# Select other Plotly events by specifying kwargs
-fig = px.line(x=[1], y=[1])
-selected_points = plotly_events(fig, click_event=True, hover_event=True)
+st.pyplot(fig)
